98c4cd9d75d0145bf38b803440ece568ea6b9cff
P17532529 <p17532529@gmail.com>
2023-06-26 12:09:06 +0530

    master added

--- src/App.tsx
+++ src/App.tsx
@@ -25,9 +25,11 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+
       <p className="read-the-docs">
         Dev
       </p>
+      <h1>Master</h1>
     </>
   )
 }
